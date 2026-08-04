383ce6c4c67cf7dd6f028f6247de18274c927cac
INDRAKUMAR2005 <indrakumar2005@gmail.com>
2026-08-04 12:28:21 +0530

    Positve,Negative,Zero

---
+++ track/MO2 - Introduction to java/posneg.java
@@ -0,0 +1,14 @@
+public class posneg {
+    public static void main(String[] args) {
+        int a = -25;
+        if (a > 0) {
+            System.out.print("Positive");
+        } else if (a < 0) {
+            System.out.print("Negative");
+        } else if (a == 0) {
+            System.out.print("Zero");
+
+        }
+    }
+
+}
