9423f17350c8f81c00ff53d171689a37b702249d
INDRAKUMAR2005 <indrakumar2005@gmail.com>
2026-08-04 11:28:19 +0530

    prinln assessment

---
+++ track/MO2 - Introduction to java/week.java
@@ -0,0 +1,11 @@
+class week {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("friday");
+        System.out.println("Satuday");
+        System.out.println("Sunday");
+    }
+}
