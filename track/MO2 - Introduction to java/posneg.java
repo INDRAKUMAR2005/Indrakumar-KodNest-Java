e0d8ce897509f65639c3d45312f6e41e803a1b2c
INDRAKUMAR2005 <indrakumar2005@gmail.com>
2026-08-04 13:06:04 +0530

    ifelse ladder

--- track/MO2 - Introduction to java/posneg.java
+++ track/MO2 - Introduction to java/posneg.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class posneg {
     public static void main(String[] args) {
-        int a = -25;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
         if (a > 0) {
             System.out.print("Positive");
         } else if (a < 0) {
